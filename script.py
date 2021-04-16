691ec76fcdc86125d79843ad9b064a041f6b2df9
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-04-16 15:06:06 +0530

    Create script.py

--- script.py
+++ script.py
@@ -7,10 +7,54 @@ class Script(object):
 
 
     HELP_MSG = """
-എന്നെ എന്റെ ഓണർക്ക് മാത്രെമേ കൺട്രോൾ ചെയ്യാൻ പറ്റുകയൊള്ളു...😜
+<i>Add me as admin in your group and start filtering :)</i>
+
+
+<b>Basic Commands;</b>
+
+/start - Check if I'm alive!
+/help - Command help
+/about - Something about me!
+
+
+<b>Filter Commands;</b>
+
+<code>/add name reply</code>  -  Add filter for name
+
+<code>/del name</code>  -  Delete filter
+
+<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
+
+<code>/viewfilters</code>  -  List all filters in chat
+
+
+<b>Connection Commands;</b>
+
+<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
+<code>/connect</code> in groups.
+
+<code>/connections</code>  -  Manage your connections.
+
+
+<b>Extras;</b>
+
+/status  -  Shows current status of your bot (Auth User Only)
+
+/id  -  Shows ID information
+
+<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
+
+
+<b>© @VKPROJECTS</b>
 """
 
 
-    ABOUT_MSG = """എന്നെ എന്റെ ഓണർക്ക് മാത്രെമേ കൺട്രോൾ ചെയ്യാൻ പറ്റുകയൊള്ളു...😜
+    ABOUT_MSG = """⭕️<b>My Name : Filter Bot</b>
+
+⭕️<b>Creater :</b> @VKPROJECTS  
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
 """
